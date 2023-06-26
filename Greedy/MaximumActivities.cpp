d4ddcca7c67a950bf660d4ca067b5c83f515914e
Babykrishna Rayaguru <79391348+bkrayaguru931@users.noreply.github.com>
2023-06-26 23:57:15 +0530

    Easy-Medium

--- Greedy/MaximumActivities.cpp
+++ Greedy/MaximumActivities.cpp
@@ -4,23 +4,18 @@ int maximumActivities(vector<int> &start, vector<int> &finish) {
     // Write your code here.
     vector<pair<int, int>> vp;
     int n=start.size();
-
     for (int i=0; i<n; i++)
         vp.push_back({finish[i], start[i]});
-
     sort(vp.begin(), vp.end());
-
-    int count=1;
-    int endLimit=vp[0].first;
-
+    int cnt=1;
+    int endLim=vp[0].first;
     for (int i=1; i<n; i++) {
-        int s=vp[i].second, e=vp[i].first;
-
-        if (s>=endLimit) {
-        count++;
-        endLimit=e;
+        int s=vp[i].second;
+        int e=vp[i].first;
+        if (s>=endLim) {
+        cnt++;
+        endLim=e;
         }
     }
-
-    return count;
+    return cnt;
 }
