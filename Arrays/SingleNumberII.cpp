876771be435f32d720574cfe931e01c29065a1cf
Babykrishna Rayaguru <79391348+bkrayaguru931@users.noreply.github.com>
2023-07-04 21:31:59 +0530

    notInSheetbutSimilar

---
+++ Arrays/SingleNumberII.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // unordered_map<int, int> m;       
+        // for(auto x: nums){
+        //     m[x]++;
+        // }
+        // for(auto x: m){
+        //     if(x.second == 1){
+        //         return x.first;
+        //     }
+        // }
+        // return -1;
+
+        int a = 0,b = 0;
+        for(auto x:nums){
+            a=(a^x) & ~b;
+            b=(b^x)& ~a;
+        }
+        return a;
+    }
+};
